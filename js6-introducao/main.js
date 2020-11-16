50fc5a7f85a03a3793aec6607307f88590d10270
Antonio Lima <antonio55senac@gmail.com>
2020-11-16 00:20:34 +0000

    Substituição de Valor

--- js6-introducao/main.js
+++ js6-introducao/main.js
@@ -1,10 +1,11 @@
 var nome = "Antonio Lima";
 var idade = 21;
 var idade2 = 10;
+var frase = "BRASIL";
 
 
 //alert(idade + idade2);
 console.log(nome);
 console.log(idade+idade2);
-
+console.log(frase.replace("BRASIL","Japão"));
 
