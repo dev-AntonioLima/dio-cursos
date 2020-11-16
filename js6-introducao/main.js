3307608aba438b8ed560ad05fe42e37600b81aab
Antonio Lima <antonio55senac@gmail.com>
2020-11-16 00:30:36 +0000

    adicionar/remover

--- js6-introducao/main.js
+++ js6-introducao/main.js
@@ -9,6 +9,13 @@ console.log(nome);
 console.log(idade+idade2);
 console.log(frase.replace("BRASIL","Japão"));
 
+
+
 var list = ["maça", "pera", "laranja"];
+//Remover elemente da array.
+list.pop();
+//Adicionar elemeto a array.
+list.push("uva");
+
 
 console.log(list);
